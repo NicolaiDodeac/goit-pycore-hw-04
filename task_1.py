d91291e1e80ef8820e77ce6dc3eeff3d751cd25f
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2025-03-11 17:33:30 +0000

    task 2 ready

--- task_1.py
+++ task_1.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 def total_salary(path):
     try:
         with open(path, 'r') as fh:
---
+++ task_2.py
@@ -0,0 +1,19 @@
+def get_cats_info(path):
+    cats_list = []
+    try:
+        with open(path, "r", encoding='utf-8') as cats: 
+            for cat in cats:
+                try:
+                    id_info, name_info, age_info = cat.strip().split(",")
+                    cat_info = {"id": id_info, "name": name_info, "age": age_info}
+                    cats_list.append(cat_info)
+                except ValueError:
+                    print(f"Некоректний формат рядка: '{cat}'")
+        return cats_list
+    
+    except FileNotFoundError:
+        print(f"Файл не знайдено, перевірте шлях: {path}")
+        return []
+
+cats_info = get_cats_info("./cats_file.txt")
+print(cats_info)

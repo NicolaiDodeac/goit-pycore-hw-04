fd4f1e6f2e7bff3b0ee59b4b66151a1718d783e2
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2025-03-11 14:28:13 +0000

    1 - done

---
+++ task_1.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+def total_salary(path):
+    try:
+        with open(path, 'r') as fh:
+            lines_amount = 0
+            salaries = []
+            for el in fh.readlines():
+                lines_amount +=1
+                _, salary = el.strip().split(",")
+                salaries.append(float(salary))
+        total = sum(salaries)
+        average = total / lines_amount if lines_amount else 0
+        return (int(total), int(average))
+    except FileNotFoundError:
+        print(f"Файл не знайдено: {path}")
+        return "?", "?"
+
+total, average = total_salary("./salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
